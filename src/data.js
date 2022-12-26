35040cca1b594ecae6595a6363a1395fb5fbd521
NataRuto <13natik@mail.ru>
2022-12-26 23:53:47 +0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,51 @@
+export const data = [
+    {
+        id: 1,
+        name: "Татьяна Иванова",
+        description: "Всегда остаюсь оптимисткой. Верю в людей и мечтаю познакомиться с достойным мужчиной, который нуждается в заботе, любви и нежности.",
+        age: 32,
+        image: "https://images.unsplash.com/photo-1567532939604-b6b5b0db2604?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8d29tZW4lMjBmYWNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+    },
+
+    {
+        id: 2,
+        name: "Ольга Егорова",
+        description: "Хочу остаться загадкой, чтобы не писать банальности из желания поскорее выйти замуж. Лучше я подожду мужчину, для которого наше знакомство будет удачей, и наша семья станет маленькой домашней крепостью.",
+        age: 30,
+        image: "https://images.unsplash.com/photo-1614436086835-d18683eb24f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fHdvbWVuJTIwZmFjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+    },
+
+    {
+        id: 3,
+        name: "Артем Соколов",
+        description: "Моя жизнь полна интересных событий. Единственное, чего мне не хватает – любимая девушка рядом.",
+        age: 33,
+        image: "https://images.unsplash.com/photo-1520409364224-63400afe26e5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fG1lbiUyMGZhY2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+    },
+
+    {
+        id: 4,
+        name: "Егор Арбузов",
+        description: "Специалист по ИТ-технологиям. Ищу интересную девушку любой внешности. Обожаю прогулки по вечерам и спорт, слабость – вкусная домашняя еда.",
+        age: 36,
+        image: "https://images.unsplash.com/photo-1595956553066-fe24a8c33395?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bWVuJTIwZmFjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+    },
+
+    {
+        id: 5,
+        name: "Ирина Солнцева",
+        description: "Дружелюбная и романтичная девушка. Я сентиментальна и не люблю ложь. Люблю дождь и книги, мечтаю о мужчине, для которого семья – это главное.",
+        age: 27,
+        image: "https://images.unsplash.com/photo-1617077644557-64be144aa306?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fHdvbWVuJTIwZmFjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+    },
+
+    {
+        id: 6,
+        name: "Кирилл Богданов",
+        description: "Не люблю спорт – вместо этого я трачу время на инвестиции в будущее, которое мечтаю разделить с тобой. Кстати, я имел в виду просмотр футбола, а тренажерный зал – это святое. Я буду демонстрировать тебе свою силу, чтобы защищать и оберегать хрупкую тебя.",
+        age: 29,
+        image: "https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1lbiUyMGZhY2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+    }
+
+
+]
